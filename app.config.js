d9388983e8330dcf8fdb980cdfbf866b3c78cec2
Raymond Willey <rwilley@Raymonds-MacBook-Pro.local>
2025-03-05 12:25:34 -0500

    Configure firebase and secrets

---
+++ app.config.js
@@ -0,0 +1,16 @@
+// app.config.js
+export default ({ config }) => {
+  return {
+    ...config,
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      standardHashKey: process.env.STANDARD_HASH_KEY,
+      profileHashKey: process.env.PROFILE_HASH_KEY
+    },
+  };
+};
