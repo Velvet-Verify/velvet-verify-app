be44460ff96c33143af8798ae76db8d82bc9742c
Raymond Willey <rwilley@Raymonds-MacBook-Pro.local>
2025-05-11 19:09:36 -0400

    Test profile editor

--- components/profile/ProfileHeader.tsx
+++ components/profile/ProfileHeader.tsx
@@ -79,7 +79,7 @@ export function ProfileHeader({
           <View style={theme.profileHeaderActionsRow}>
             {onEditProfile && (
               <TouchableOpacity onPress={onEditProfile}>
-                <Text style={theme.profileHeaderLink}>Edit Profile</Text>
+                <Text style={theme.profileHeaderLink}>Profile Editor</Text>
               </TouchableOpacity>
             )}
 
